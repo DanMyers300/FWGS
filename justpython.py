76649eb30cf5e20a61dd97473ba8dbf665b1fbc6
Colloke <github.p5x18@simplelogin.co>
2023-04-26 11:09:45 -0500

    tried to do it with just python

--- justpython.py
+++
@@ -1,7 +0,0 @@
-with open('test.txt', 'r') as f:
-    for line in f:
-        # Split the line into a list of values
-        values = line.strip().split(',')
-
-        # Process the values as needed
-        print(values)
