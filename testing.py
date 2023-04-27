3c5feedceb550f9c5ba01430d52b845eafc9e1e2
Colloke <github.p5x18@simplelogin.co>
2023-04-27 11:14:06 -0500

    added .env file and git ignore

--- testing.py
+++ testing.py
@@ -1,23 +1,72 @@
-# create an empty dictionary to store the data
-data = {}
+from langchain import HuggingFaceHub
+from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+import textwrap
+from langchain.document_loaders import TextLoader
+import requests
+import os
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "HUGGINGFACEHUB_API_TOKEN"
 
-# open the text file for reading
-with open("test.txt", "r") as file:
-    # read each line in the file
-    for line in file:
-        # remove any leading or trailing whitespace from the line
-        line = line.strip()
 
-        # check if the line contains a colon
-        if ":" in line:
-            # split the line into key-value pairs
-            key, value = line.split(":", 1)
-            # remove any leading or trailing whitespace from the key and value
-            key = key.strip()
-            value = value.strip()
-            # add the key-value pair to the dictionary
-            data[key] = value
+url = "https://raw.githubusercontent.com/hwchase17/langchain/master/docs/modules/state_of_the_union.txt"
+res = requests.get(url)
+with open("state_of_the_union.txt", "w") as f:
+    f.write(res.text)
 
-# print the dictionary as a table
-for key, value in data.items():
-    print("{:<30} {}".format(key, value))
+# Document Loader
+loader = TextLoader('./state_of_the_union.txt')
+documents = loader.load()
+
+
+def wrap_text_preserve_newlines(text, width=110):
+    # Split the input text into lines based on newline characters
+    lines = text.split('\n')
+
+    # Wrap each line individually
+    wrapped_lines = [textwrap.fill(line, width=width) for line in lines]
+
+    # Join the wrapped lines back together using newline characters
+    wrapped_text = '\n'.join(wrapped_lines)
+
+    return wrapped_text
+
+
+# print(wrap_text_preserve_newlines(str(documents[0])))
+
+
+# Text Splitter
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+docs = text_splitter.split_documents(documents)
+
+len(docs)
+
+docs[0]
+
+# Embeddings
+
+embeddings = HuggingFaceEmbeddings()
+
+# Vectorstore: https://python.langchain.com/en/latest/modules/indexes/vectorstores.html
+
+db = FAISS.from_documents(docs, embeddings)
+
+query = "What did the president say about the Supreme Court"
+docs = db.similarity_search(query)
+
+print(wrap_text_preserve_newlines(str(docs[0].page_content)))
+
+
+llm = HuggingFaceHub(repo_id="google/flan-t5-xl",
+                     model_kwargs={"temperature": 0, "max_length": 512})
+chain = load_qa_chain(llm, chain_type="stuff")
+
+query = "What did the president say about the Supreme Court"
+docs = db.similarity_search(query)
+chain.run(input_documents=docs, question=query)
+
+
+query = "What did the president say about economy?"
+docs = db.similarity_search(query)
+chain.run(input_documents=docs, question=query)
