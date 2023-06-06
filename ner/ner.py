4f161f1eb0e70da3e64e7324064bdb4b0add6c55
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-06 03:37:21 +0000

    added email parser

--- ner/ner.py
+++ ner/ner.py
@@ -14,5 +14,11 @@ def open_file():
 nlp = spacy.load("en_core_web_lg")
 doc = nlp(open_file())
 
-for ent in doc.ents:
-    print(ent.text, ent.label_)
+# for ent in doc.ents:
+#     print(ent.text, ent.label_)
+
+EMAIL_PATTERN = [{"LIKE_EMAIL": True}]
+matcher.add("EMAIL_ADDRESS", [EMAIL_PATTERN])
+Emailmatches = matcher(doc)
+for match in Emailmatches[:10]:
+    print(match, doc[match[1]])
