017ef2d02064f5f974fc887553197f0284759c73
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-06 01:15:20 +0000

    added main file to run other scripts

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
 """
-NER
-
-TO-DO:
-
-    1)
-    2)
-    3)
+1) Run pdf processor
+2) Run NER
 """
+import subprocess
 
-from pypdf import PdfReader
+# Run pdf_processor.py
+PDF_PROCESSOR = "./ner/pdf_processor.py"
+subprocess.run(["python", PDF_PROCESSOR], check=True)
 
-class PDFProcessor:
-    "Object made to process PDFs"
-    def __init__(self):
-        self.pdf_file_name = None
-        self.non_empty_lines = None
-
-    def get_pdf_file_name(self):
-        "Get the name of the file from the user"
-        self.pdf_file_name = input("Enter the name of the PDF file: ")
-        # pylint: disable=C0301
-        self.pdf_file_name = "/workspaces/Fort_Worth_Gasket_And_Supply_Project/data/" + self.pdf_file_name
-        if not self.pdf_file_name.endswith(".pdf"):
-            self.pdf_file_name += ".pdf"
-
-    def extract_text_from_pdf(self):
-        "Extract the text"
-        reader = PdfReader(self.pdf_file_name)
-
-        output_file = open("data/output.txt", "w", encoding="utf-8")
-
-        for page in reader.pages:
-            output_file.write(page.extract_text())
-        output_file.close()
-
-    def read_text_file(self):
-        "Add the non empty lines to a file and a string"
-        with open("data/output.txt", encoding="utf-8") as file:
-            lines = [line.strip() for line in file]
-            self.non_empty_lines = [line for line in lines if line != ""]
-        return "\n".join(self.non_empty_lines)
-
-# Run the pdf Processor
-processor = PDFProcessor()
-processor.get_pdf_file_name()
-processor.extract_text_from_pdf()
-OUTPUT_STRING = processor.read_text_file()
-print(OUTPUT_STRING)
+# Run ner.py
+NER = "./ner/ner.py"
+subprocess.run(["python", NER], check=True)
