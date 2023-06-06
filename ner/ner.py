1add21768f59dced0d9d857f5beb3aa6138e9744
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-06 03:06:14 +0000

    list entities and labels

--- ner/ner.py
+++ ner/ner.py
@@ -5,8 +5,14 @@ NER
 # import random
 import spacy
 
+def open_file():
+    "Open corpus of text"
+    with open("data/corpus.txt", "r", encoding="utf-8") as file:
+        contents = file.read()
+        return contents
+
 nlp = spacy.load("en_core_web_lg")
-doc = nlp(test)
+doc = nlp(open_file())
 
 for ent in doc.ents:
     print(ent.text, ent.label_)
