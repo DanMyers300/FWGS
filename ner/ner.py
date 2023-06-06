ba2512e2a55aeca59b4fc2f92c4792c042c744ad
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-06 08:34:09 +0000

    Dates succesful

--- ner/ner.py
+++ ner/ner.py
@@ -45,7 +45,7 @@ class Emails:
         results = []
         for match in self.email_matches[:10]:
             email_address = str(doc_object[match[1]])
-            results.append({"email": email_address})
+            results.append({"label": "email", "text": email_address})
             print(match, email_address)
 
         with open(output_file, "w", encoding="utf-8") as file:
@@ -73,9 +73,38 @@ class URLs:
         urls = []
         for token in base_large_doc:
             if token.like_url:
-                urls.append(token.text)
+                urls.append({"label": "URL", "text": token.text})
                 print(token.text)
+        with open(
+            "/workspaces/Fort_Worth_Gasket_And_Supply_Project/data/urls.json",
+            "w",
+            encoding="utf-8",
+        ) as file:
+            json.dump(urls, file)
         return urls
+
+class Dates:
+    "Extract dates"
+    def __init__(self):
+        self.dates = []
+        self.pattern = r"\b(0[1-9]|1[0-2])/(0[1-9]|1[0-9]|2[0-9]|3[01])/(\d{4})\b"
+
+    def parse_dates(self):
+        "Parse dates from text"
+        matcher = Matcher(base_nlp_large.vocab)
+        pattern_list = [[{"TEXT": {"REGEX": self.pattern}}]]
+        matcher.add("DATE", pattern_list)
+        matches = matcher(base_large_doc)
+
+        for match in matches:
+            _, start, end = match
+            span = base_large_doc[start:end]
+            self.dates.append({"label": "DATE", "text": span.text})
+
+        with open('/workspaces/Fort_Worth_Gasket_And_Supply_Project/data/outputs_json/dates.json', 'w', encoding="utf-8") as file:
+            json.dump(self.dates, file)
+
+        return self.dates
 
 
 # --- Extract entities --- #
@@ -83,4 +112,5 @@ class URLs:
 #     base_large_doc, "/workspaces/Fort_Worth_Gasket_And_Supply_Project/data/emails.json"
 # )
 # Addresses().extract_addresses()
-URLs().parse_urls()
+# URLs().parse_urls()
+Dates().parse_dates()
