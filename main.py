3d26f497d0e3f801718ff824bacfdbdd858e8808
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-06 00:31:12 +0000

    turned the pdf loader into an object

--- main.py
+++ main.py
@@ -1,23 +1,46 @@
 """
 NER
+
+TO-DO:
+
+    1)
+    2)
+    3)
 """
 
 # from pypdf import PdfReader
 
-# PDFFileName = input("Enter the name of the PDF file: ")
-# PDFFileName = "/workspaces/Fort_Worth_Gasket_And_Supply_Project/" + PDFFileName
-# if not PDFFileName.endswith(".pdf"):
-#     PDFFileName += ".pdf"
+class PDFProcessor:
+    "A PDF loader"
+    def __init__(self):
+        self.pdf_file_name = None
 
-# # Extract the information from the pdf and save it as a text file.
-# reader = PdfReader(PDFFileName)
+    def get_pdf_file_name(self):
+        "Request name from user"
+        self.pdf_file_name = input("Enter the name of the PDF file: ")
+        self.pdf_file_name = "/workspaces/Fort_Worth_Gasket_And_Supply_Project/" + self.pdf_file_name
+        if not self.pdf_file_name.endswith(".pdf"):
+            self.pdf_file_name += ".pdf"
 
-# output_file = open("output.txt", "w", encoding="utf-8")
+    def extract_text_from_pdf(self):
+        "Extract the text"
+        reader = PdfReader(self.pdf_file_name)
 
-# for page in reader.pages:
-#     output_file.write(page.extract_text())
-# output_file.close()
+        output_file = open("output.txt", "w", encoding="utf-8")
 
-# with open("output.txt", encoding="utf-8") as file:
-#     lines = [line.strip() for line in file]
-#     non_empty_lines = [line for line in lines if line != ""]
+        for page in reader.pages:
+            output_file.write(page.extract_text())
+        output_file.close()
+
+    def read_text_file(self):
+        "print the text to a file"
+        with open("output.txt", encoding="utf-8") as file:
+            lines = [line.strip() for line in file]
+            non_empty_lines = [line for line in lines if line != ""]
+            # Do something with non_empty_lines
+
+# Usage example:
+processor = PDFProcessor()
+processor.get_pdf_file_name()
+processor.extract_text_from_pdf()
+processor.read_text_file()
