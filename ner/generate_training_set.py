f09e9d3d60f45fce7039ff57fd5f4c5dc44ddf90
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-08 08:41:47 +0000

    created an RFQ training set

--- ner/generate_training_set.py
+++ ner/generate_training_set.py
@@ -1,31 +1,61 @@
-"""
-Generate Training Set
-"""
 import json
+import spacy
 
-# import spacy
-# from spacy.lang.en import English
-# from spacy.pipeline import EntityRuler
+# Build upon the spaCy Small Model
+nlp = spacy.load("en_core_web_lg")
 
-MAIN_FILE = "data/formatted_training_data/RFQ.json"
+def open_file(input_file):
+    "Open a text file"
+    with open(input_file, "r", encoding="utf-8") as opened_file:
+        open_text = opened_file.read()
+    return open_text
 
+# Load the patterns from the JSON file
+PATTERNS_FILE = "data/formatted_training_data/RFQ.json"
+with open(PATTERNS_FILE, "r", encoding="utf-8") as file:
+    patterns_data = json.load(file)
 
-def load_data(file):
-    "Load Data"
-    with open(file, "r", encoding="utf-8") as file:
-        load__json_data = json.load(file)
-    return load__json_data
+# Extract the patterns from the loaded data
+patterns = patterns_data["patterns"]
 
+# Sample text
+TEXT = open_file("data/outputs/rfq_dump.txt")
 
-def create_training_data(file, label):
-    "Create Training Data"
-    data = load_data(file)
-    patterns = []
-    for item in data:
-        pattern = {"label": label, "pattern": item}
-        patterns.append(pattern)
-    return patterns
+corpus = []
 
+doc = nlp(TEXT)
+for sent in doc.sents:
+    corpus.append(sent.text)
 
-training_patterns = create_training_data(MAIN_FILE, "RFQ")
-print(training_patterns)
+# Build upon the spaCy Small Model
+nlp = spacy.blank("en")
+
+# Create the EntityRuler
+ruler = nlp.add_pipe("entity_ruler")
+
+# Add the loaded patterns to the EntityRuler
+ruler.add_patterns(patterns)
+
+TRAIN_DATA = []
+
+# Iterate over the corpus
+for sentence in corpus:
+    doc = nlp(sentence)
+
+    # Initialize an empty list to store entities and their corresponding text segments
+    entity_segments = []
+
+    # Extract entities and their corresponding text segments
+    for ent in doc.ents:
+        entity_segments.append([sentence[ent.start_char:ent.end_char], ent.label_])
+
+    # Print the entity and its corresponding text segment
+    for segment, entity in entity_segments:
+        print("Text Segment:", segment)
+        print("Entity:", entity)
+        print()
+
+    # Append the sentence and entities to the training data
+    if entity_segments:
+        entities = [{"start": ent.start_char, "end": ent.end_char, "label": ent.label_} for ent in doc.ents]
+        TRAIN_DATA.append([sentence, {"entities": entities}])
