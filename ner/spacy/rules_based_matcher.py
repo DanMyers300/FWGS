fff38728f1abf604118cc9b1fd0d0fb8c1c70ff9
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-13 17:01:31 +0000

    pattern working but not with the json

--- ner/spacy/rules_based_matcher.py
+++ ner/spacy/rules_based_matcher.py
@@ -8,17 +8,17 @@ def open_file(input_file):
         open_text = opened_file.read()
     return open_text
 
-PATTERNS_FILE = "data/formatted_training_data/RFQ.json"
-with open(PATTERNS_FILE, "r", encoding="utf-8") as file:
-    patterns_data = json.load(file)
-patterns = patterns_data["patterns"]
+# PATTERNS_FILE = "data/formatted_training_data/RFQ.json"
+# with open(PATTERNS_FILE, "r", encoding="utf-8") as file:
+#     patterns_data = json.load(file)
+# patterns = patterns_data["patterns"]
 
 TEXT = open_file("data/outputs/rfq_dump.txt")
 
 nlp = spacy.load("en_core_web_sm")
 matcher = Matcher(nlp.vocab)
 # Add match ID "HelloWorld" with no callback and one pattern
-pattern = patterns
+pattern = [{"TEXT": "600114XXXX"}]
 matcher.add("RFQ", [pattern])
 
 doc = nlp(TEXT)
