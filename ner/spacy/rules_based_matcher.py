48987ed98392b0cb1931be5307e3a6718861fe70
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-06-13 19:46:06 +0000

    updated rules_based_matching

--- ner/spacy/rules_based_matcher.py
+++ ner/spacy/rules_based_matcher.py
@@ -1,7 +1,5 @@
 """
-This script uses spaCy's rule-based matcher to extract RFQs from a text file.
-This uses matcher so it will only work off the patterns given. 
-However, it can be used to train a custom model.
+This file outputs the corpus of text with the entities labeled in the training format
 """
 import json
 import spacy
@@ -17,40 +15,29 @@ def open_file(input_file):
 
 TEXT = open_file("data/outputs/rfq_dump.txt")
 PATTERNS_FILE = "data/formatted_training_data/RFQ.json"
-
 TEXT_OUTPUT_FILE = "data/formatted_training_data/RFQ/labeled_rfqs.txt"
-ENT_OUTPUT_FILE = "data/formatted_training_data/RFQ/extracted_rfqs.txt"
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(TEXT)
 
 with open(PATTERNS_FILE, "r", encoding="utf-8") as file:
     patterns_data = json.load(file)
 patterns = patterns_data["patterns"]
 
-nlp = spacy.load("en_core_web_sm")
 matcher = Matcher(nlp.vocab)
 matcher.add("RFQ", patterns)
-doc = nlp(TEXT)
 matches = matcher(doc)
 
-entities = []
-rfqs = []
-for match_id, start, end in matches:
-    entities.append((start, end - 1, doc.vocab.strings[match_id]))
-    rfqs.append(doc[start:end].text)
+TRAIN_DATA = []
+for sent in doc.sents:
+    entities = []
+    for match_id, start, end in matches:
+        if start >= sent.start and end <= sent.end:
+            entities.append(
+                [start - sent.start, end - sent.start - 1, doc.vocab.strings[match_id]]
+            )
+    TRAIN_DATA.append([sent.text, {"entities": entities}])
 
 with open(TEXT_OUTPUT_FILE, "w", encoding="utf-8") as f:
-    for i, token in enumerate(doc):
-        f.write(token.text_with_ws)
-        for start, end, label in entities:
-            if i == end:
-                f.write(f"[({start}, {end}, {label})] ")
-    f.write("\n\nExtracted RFQs:\n")
-    for rfq in rfqs:
-        f.write(f"{rfq}\n")
-
-with open(ENT_OUTPUT_FILE, "w", encoding="utf-8") as f:
-    for rfq in rfqs:
-        f.write(f"{rfq}\n")
-
-print("Extracted RFQs:")
-for rfq in rfqs:
-    print(rfq)
+    for item in TRAIN_DATA:
+        f.write(f"{item}\n")
