86c4460f3d6700a6c82278e4ac8704a458acf3a4
DanMyers300 <contact+github@danmyers.net>
2023-06-27 11:25:36 -0500

    created word vectors

--- src/vectors.py
+++ src/vectors.py
@@ -31,4 +31,37 @@ for sent in doc.sents:
     sentence = sent.text.translate(str.maketrans('', '', string.punctuation))
     words = sentence.split()
     sentences.append(words)
-print(sentences)
+
+def create_wordvecs(corpus, model_name):
+    print(len(corpus))
+
+    phrases = Phrases(corpus, min_count=30, progress_per=10000)
+    print("Made phrases")
+
+    bigram = Phraser(phrases)
+    print("Made bigram")
+
+    sentences = phrases[corpus]
+    print("Found sentences")
+    word_freq = defaultdict(int)
+
+    for sent in sentences:
+        for i in sent:
+            word_freq[i] += 1
+    
+    print(len(word_freq))
+
+    print("Training model now...")
+
+    w2v_model = Word2Vec(min_count=1,
+                         window=2,
+                         vector_size=10,
+                         sample=6e-5,
+                         alpha=0.03,
+                         min_alpha=0.0007,
+                         negative=20)
+    w2v_model.build_vocab(sentences, progress_per=10000)
+    w2v_model.train(sentences, total_examples=w2v_model.corpus_count, epochs=30, report_delay=1)
+    w2v_model.wv.save_word2vec_format(f"data/outputs/{model_name}.txt")
+create_wordvecs(sentences, "rfq_wordvecs")
+
