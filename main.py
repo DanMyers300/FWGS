89dac93cce6aec1c5db1dd7bfa0c18b74bccfe06
DanMyers300 <contact+github@danmyers.net>
2023-09-09 16:50:31 -0500

    Error handling in main.py and pdf_processor.py

--- main.py
+++ main.py
@@ -5,11 +5,20 @@
 
 import subprocess
 
+def run_subprocess(command, description):
+    try:
+        subprocess.run(command, check=True)
+        print(f"{description} completed successfully.")
+    except subprocess.CalledProcessError as e:
+        print(f"Error while running {description}: {e}")
+        raise Exception(f"{description} failed")
+
 # Run pdf_processor.py
 PDF_PROCESSOR = "src/pdf_processor.py"
-subprocess.run(["python", PDF_PROCESSOR], check=True)
+run_subprocess(["python", PDF_PROCESSOR], "PDF Processor")
 
 # Run ner.py
 NER = "src/runNER.py"
-subprocess.run(["python", NER], check=True)
-raise Exception("NER failed")
+run_subprocess(["python", NER], "NER")
+
+# If NER fails, an exception has already been raised in the `run_subprocess` function.
--- src/pdf_processor.py
+++ src/pdf_processor.py
@@ -8,7 +8,6 @@ import os
 import re
 from pypdf import PdfReader
 
-
 class PDFProcessor:
     "Object made to process PDFs"
 
@@ -18,59 +17,56 @@ class PDFProcessor:
 
     def get_pdf_file_names(self):
         "Get the names of all PDF files in the directory"
-        pdf_files = [
-            file
-            for file in os.listdir(
-                "data/"
-            )
-            if file.endswith(".pdf")
-        ]
-        self.pdf_file_names = [
-            os.path.join("data/", file)
-            for file in pdf_files
-        ]
-
-    # def get_pdf_file_name(self):
-    #     "Get the name of the file from the user"
-    #     self.pdf_file_name = input("Enter the name of the PDF file: ")
-    #     # pylint: disable=C0301
-    #     self.pdf_file_name = (
-    #         "data/"
-    #         + self.pdf_file_name
-    #     )
-    #     if not self.pdf_file_name.endswith(".pdf"):
-    #         self.pdf_file_name += ".pdf"
+        try:
+            pdf_files = [
+                file
+                for file in os.listdir("data/")
+                if file.endswith(".pdf")
+            ]
+            self.pdf_file_names = [
+                os.path.join("data/", file)
+                for file in pdf_files
+            ]
+        except OSError as e:
+            print(f"Error while getting PDF file names: {e}")
 
     def extract_text_from_pdf(self):
         "Extract the text from all PDF files"
-        output_file = open(
-            "data/corpus.txt",
-            "w",
-            encoding="utf-8",
-        )
+        try:
+            output_file = open(
+                "data/corpus.txt",
+                "w",
+                encoding="utf-8",
+            )
 
-        for pdf_file in self.pdf_file_names:
-            reader = PdfReader(pdf_file)
+            for pdf_file in self.pdf_file_names:
+                try:
+                    reader = PdfReader(pdf_file)
 
-            for i, page in enumerate(reader.pages):
-                if i + 1 not in [2, 3, 4]:  # Skip pages 2, 3, and 4
-                    text = page.extract_text()
-                    text = re.sub(
-                        r"\n+", " ", text
-                    )  # Replace multiple newline characters with spaces
-                    text = re.sub(
-                        r"\s{2,}", " ", text
-                    )  # Replace multiple spaces with a single space
-                    text = re.sub(
-                        r"^\s*\n", "", text, flags=re.MULTILINE
-                    )  # Remove blank lines
+                    for i, page in enumerate(reader.pages):
+                        if i + 1 not in [2, 3, 4]:  # Skip pages 2, 3, and 4
+                            text = page.extract_text()
+                            text = re.sub(
+                                r"\n+", " ", text
+                            )  # Replace multiple newline characters with spaces
+                            text = re.sub(
+                                r"\s{2,}", " ", text
+                            )  # Replace multiple spaces with a single space
+                            text = re.sub(
+                                r"^\s*\n", "", text, flags=re.MULTILINE
+                            )  # Remove blank lines
 
-                    output_file.write(text)
+                            output_file.write(text)
 
-        output_file.close()
+                except Exception as e:
+                    print(f"Error processing {pdf_file}: {e}")
 
+            output_file.close()
+        except OSError as e:
+            print(f"Error while writing to output file: {e}")
 
 # Run the pdf Processor
 processor = PDFProcessor()
 processor.get_pdf_file_names()
 processor.extract_text_from_pdf()
+
