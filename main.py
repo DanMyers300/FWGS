143744a5d5230881f87effee196e8263c2631f5e
DanMyers300 <contact+github@danmyers.net>
2023-09-09 21:01:00 -0500

    restructuring code

--- main.py
+++ main.py
@@ -3,7 +3,8 @@
 2) Run NER applications
 """
 from src.pdf_processor import PDFProcessor
-from src import runNER
+from src.runNER import EMAILS, URLs, DATES, ADDRESSES, RFQ, CODED_NOTES, COMBINE_OUTPUTS
+
 # Run pdf_processor.py
 processor = PDFProcessor()
 processor.get_pdf_file_names()
@@ -13,48 +14,26 @@ processor.extract_text_from_pdf()
 # --- Run the extraction --- #
 #
 
-#------------------------------
-
 EMAILS().extract_emails(
    "data/outputs/emails.json"
 )
-
-#------------------------------
-
 URLs().extract_urls(
         "data/outputs/urls.json"
 )
-
-#------------------------------
-
 DATES().extract_dates(
         "data/outputs/dates.json",
 )
-
-#------------------------------
-
 ADDRESSES().extract_addresses(
         "data/outputs/addresses.json",
 )
-
-#------------------------------
-
 RFQ().extract_rfq(
         "data/models/rfq_model/model-best",
         "data/outputs/addresses.json",
         "data/outputs/rfq.json",
 )
-
-#------------------------------
-
 CODED_NOTES().extract_coded_notes(
         "data/base_files/csv/coded_notes.csv",
         "data/outputs/coded_notes.json",
 )
-
-#------------------------------
-
 COMBINE_OUTPUTS().combine_outputs()
-
-#------------------------------
 
