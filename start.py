7af6b08964e72f774384403022cec56cdb4708bf
DanMyers300 <contact+github@danmyers.net>
2023-09-17 14:43:19 -0500

    basic flask app

---
+++ start.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('ner.html')
+
+@app.route('/send_message', methods=['POST'])
+def send_message():
+    user_message = request.form['user_message']
+    # Here, you can add your chatbot logic to generate responses
+    # For now, let's just echo the user's message
+    bot_response = "You said: " + user_message
+    return jsonify({"bot_response": bot_response})
+
+if __name__ == '__main__':
+    app.run(debug=True)
