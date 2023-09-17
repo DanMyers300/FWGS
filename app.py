fecb75bf63fd6c8db7d4f22bb451c4b590d842d8
DanMyers300 <contact+github@danmyers.net>
2023-09-17 16:07:46 -0500

    removed duplicate code

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ def index():
 @app.route('/send_message', methods=['POST'])
 def send_message():
     user_message = request.form['user_message']
-    # Here, you can add your chatbot logic to generate responses
-    # For now, let's just echo the user's message
-    bot_response = "You said: " + user_message
-    return jsonify({"bot_response": bot_response})
 
 if __name__ == '__main__':
     app.run(debug=True)
