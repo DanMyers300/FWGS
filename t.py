8602286657c6ae62f0fb0b1320ab37dbd2fc1128
Dan <contact+github@danmyers.net>
2023-10-15 19:59:48 +0000

    working ollama bot

---
+++ t.py
@@ -0,0 +1,7 @@
+from langchain.llms import Ollama
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler 
+
+llm = Ollama(model="llama2", 
+                     callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
+llm("Who was the first person on the moon? ...")
