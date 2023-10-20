cc8b4a496f56edf68ff9901cc2d131f3179131e3
Dan <contact+github@danmyers.net>
2023-10-20 09:46:11 -0500

    ollama docker containers working!

--- src/t.py
+++ src/t.py
@@ -7,17 +7,7 @@ from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 model_name = "llama2"
 question = "Who was the first person on the moon? ..."
 
-def pull_ollama_model(model_name):
-    try:
-        command = f"ollama pull {model_name}"
-        subprocess.run(command, shell=True, check=True)
-        print(f"Sucessfully pulled {model_name}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e}")
-    except Exception as e:
-        print(f"error: {str(e)}")
-
 if __name__ == "__main__":
-    pull_ollama_model(model_name)
+    subprocess.run(f"ollama pull {model_name}", shell=True, check=True, stdout=subprocess.PIPE)
     llm = Ollama(model=model_name, callback_manager = CallbackManager([StreamingStdOutCallbackHandler()]))
     llm(question)
