62cc988b611bfc525687fcd488fcc2ed358f38d2
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-11-05 02:04:25 +0000

    updated run.sh

--- ui/run.sh
+++ ui/run.sh
@@ -8,4 +8,6 @@ apt-get install -y npm
 source ~/.config/envman/PATH.env
 curl https://webi.sh/caddy | sh
 npm ci
+cp -RPp example.env .env
 PUBLIC_API_BASE_URL='https://localhost/api' npm run build
+caddy run --envfile .env --config ./Caddyfile.localhost
