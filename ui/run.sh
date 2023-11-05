a6cae42f3fd9989c23e28655baa73000a8ac8c63
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-11-05 02:09:10 +0000

    added caddy

--- ui/run.sh
+++ ui/run.sh
@@ -6,7 +6,11 @@ apt-get install -y curl
 apt-get install -y nodejs
 apt-get install -y npm
 source ~/.config/envman/PATH.env
-curl https://webi.sh/caddy | sh
+apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
+sudo apt update
+sudo apt install caddy
 npm ci
 cp -RPp example.env .env
 PUBLIC_API_BASE_URL='https://localhost/api' npm run build
