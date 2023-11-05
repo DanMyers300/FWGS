7ab474dfb90f297f5a77dde61042aba9a9f5e876
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-11-05 01:52:43 +0000

    updated run.sh

--- ui/run.sh
+++ ui/run.sh
@@ -3,8 +3,7 @@
 apt-get update -y
 apt-get upgrade -y
 apt-get install -y curl
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
-nvm install --lts
+apt install nodejs npm
 source ~/.config/envman/PATH.env
 curl https://webi.sh/caddy | sh
 npm ci
