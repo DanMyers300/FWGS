fce8322bb0a0d36a2d99889fe9f75425a51610f3
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-11-05 01:56:07 +0000

    a

--- ui/run.sh
+++ ui/run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install -y curl
-sudo apt-get install -y nodejs npm
+apt-get update -y
+apt-get upgrade -y
+apt-get install -y curl
+apt-get install -y nodejs npm
 source ~/.config/envman/PATH.env
 curl https://webi.sh/caddy | sh
 npm ci
