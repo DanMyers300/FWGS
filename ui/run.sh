70f8e690d6e5973eb973801803bfd31b8061e896
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-11-20 16:50:06 +0000

    updated ui

--- ui/run.sh
+++
@@ -1,5 +0,0 @@
-docker stop ollama-webui || true
-docker rm ollama-webui || true
-docker build -t ollama-webui .
-docker run -d -p 3000:8080 --name ollama-webui --restart always ollama-webui
-docker image prune -f
