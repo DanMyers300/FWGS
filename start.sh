63a0bba57b249027858588c7219f0875b1218211
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2023-11-28 21:51:52 +0000

    updated start.sh

--- start.sh
+++ start.sh
@@ -17,11 +17,6 @@ echo \
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-# Install dependencies for GPU
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/gpgkey | sudo apt-key add -distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
-curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidia-container-runtime.list |\
-sudo tee /etc/apt/sources.list.d/nvidia-container-runtime.list
-sudo apt-get update
 sudo apt-get install nvidia-container-runtime
 sudo systemctl stop docker
 sudo systemctl start docker
