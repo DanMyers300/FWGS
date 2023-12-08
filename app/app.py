e25aff0bccf08e47738396c90c5b7a655655b6ae
Dan <contact+github@danmyers.net>
2023-12-08 16:25:47 -0600

    Update Flask app with JSON serialization and document ingestion

--- app/app.py
+++ app/app.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python3
-from flask import Flask, render_template, request
+import json
+from flask import Flask, render_template, request, jsonify
 from init_llm import qa, pull_model
+import subprocess
 
 app = Flask(__name__)
+
+class DocumentEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Document):
+            return obj.__dict__
+        return json.JSONEncoder.default(self, obj)
+
+app.json_encoder = DocumentEncoder
 
 @app.route("/")
 def index():
@@ -11,20 +21,25 @@ def index():
 @app.route("/ask", methods=["POST"])
 def ask_question():
     query = request.form.get("query")
-    if query.strip() == "":
-        return render_template("index.html", result="Please enter a non-empty query.")
+    if not query.strip():
+        return jsonify({"error": "Please enter a non-empty query"})
 
     res = qa(query)
     answer, docs = res["result"], res["source_documents"]
 
-    # Render the template without redirecting
-    return render_template("index.html", query=query, answer=answer, documents=docs)
+    # Serialize Document objects
+    serialized_docs = [doc.__dict__ for doc in docs]
+
+    return jsonify({"query": query, "answer": answer, "documents": serialized_docs})
 
 @app.route('/init', methods=['POST'])
 def init():
     pull_model()
-    return render_template("index.html", result="Model pulled.")
-    
+    return jsonify({"result": "Model pulled."})
+
+@app.route('/ingest', methods=['POST'])
+def ingest():
+    subprocess.run(['python', 'ingest.py'])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
