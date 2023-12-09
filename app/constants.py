ef05a514de80e0cdd608e67526b47cecc691417d
Dan <contact+github@danmyers.net>
2023-12-09 11:08:52 -0600

    Add dotenv module for environment variable loading

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,8 @@
 import os
 from chromadb.config import Settings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Define the folder for storing database
 PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', 'db')
