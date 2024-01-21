002b4424fae704af71bf8b4e4f2eefa88ff7c34a
Dan Myers <106322207+DanMyers300@users.noreply.github.com>
2024-01-21 13:47:51 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,25 +2,6 @@
 
 sudo ./docker_cleanup.sh
 git fetch && git pull origin main
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo rm -f /etc/apt/keyrings/docker.gpg
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
-echo \
-	  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-	    "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-	      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-
-sudo apt-get install nvidia-container-runtime
-sudo systemctl stop docker
-sudo systemctl start docker
-
 
 # Build and run docker containers
 sudo docker compose up --build -d
