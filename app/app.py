ed57c57262e8ba467d132e549868609123ecaf43
Dan <contact+github@danmyers.net>
2024-01-25 12:00:22 -0600

    updating app

--- app/app.py
+++ app/app.py
@@ -1,20 +1,26 @@
 from ollama import Client
-from utilities import BASE_URL, model, embeddings_model_name, persist_directory, target_source_chunks
 from flask import Flask, render_template
-from langchain_community.embeddings import OllamaEmbeddings
+
+from langchain.chains import RetrievalQA
+from langchain.embeddings import OllamaEmbeddings
 from langchain.vectorstores import Chroma
+from langchain.llms import Ollama
+
+from constants import BASE_URL, model, embeddings_model_name, PERSIST_DIRECTORY, target_source_chunks
 
 app = Flask(__name__)
-client = Client(host='http://localhost:11434')
-db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+client = Client(host=BASE_URL)
+
+embeddings = Ollama(model=embeddings_model_name)
+db = Chroma(persist_directory=PERSIST_DIRECTORY, embedding_function=embeddings)
 retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
-callbacks = [StreamingStdOutCallbackHandler()]
-llm = Ollama(model=model, callbacks=callbacks, base_url=BASE_URL)
+llm = Ollama(model=model, base_url=BASE_URL)
 
 qa = RetrievalQA.from_chain_type(
     llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True
 )
 
+# Without langchain
 def chat_generator():
     stream = client.chat(
         model='llama2',
@@ -25,12 +31,24 @@ def chat_generator():
     for chunk in stream:
         yield chunk['message']['content']
 
+# With langchain
+def handle_chat(data):
+    query = data.get('query')
+    if query:
+        res = qa(query)
+        answer, docs = res["result"], res["source_documents"]
+
+        serialized_docs = [doc.__dict__ for doc in docs]
+
+        return {"query": query, "answer": answer, "documents": serialized_docs}
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
 @app.route('/chat')
 def chat():
+    return handle_chat(request.args)
 
 @app.route('/embed')
 def embed_documents():  
--- app/constants.py
+++ app/constants.py
@@ -6,6 +6,7 @@ load_dotenv()
 
 model = os.environ.get("MODEL", "llama2:7b-chat")
 target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 4))
+embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME', 'llama2:7b')
 
 # Define the folder for storing database
 PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', 'db')
