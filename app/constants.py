4335b697fbb88071aa896af897a3d80577deef25
Dan <contact+github@danmyers.net>
2024-02-03 12:33:25 -0600

    updated embeddings model name

--- app/constants.py
+++ app/constants.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 model = os.environ.get("MODEL", "llama2")
 target_source_chunks = int(os.environ.get("TARGET_SOURCE_CHUNKS", 4))
-embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME', 'llama2:7b')
+embeddings_model_name = os.environ.get('EMBEDDINGS_MODEL_NAME', model)
 
 # Define the folder for storing database
 PERSIST_DIRECTORY = os.environ.get('PERSIST_DIRECTORY', 'db')
